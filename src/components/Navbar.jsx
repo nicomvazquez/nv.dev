717656ab43dec97f1cf0185d6617ea5aeca17bd7
nicomvazquez <vazquezpizzinicolas@gmail.com>
2024-05-28 10:57:31 -0300

    correction navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,18 +9,6 @@ function Navbar() {
       <img src="./logo.png" alt="" className="max-w-[200px] md:max-w-[270px]" />
 
       <div className="hidden md:flex gap-10 items-center ">
-        {nav.map((i) => {
-          return (
-            <a
-              key={i.title}
-              href={i.path}
-              className="text-lg font-semibold text-white hover:text-[#C44DC6] transition-all"
-            >
-              {i.title}
-            </a>
-          );
-        })}
-
         <a
           href="https://wa.link/fnh2h2"
           className=" bg-gradient text-xl flex items-center gap-3 px-5 py-3 rounded-full"
